8b8a711a684f08dd0af6d21240a91bb3961c5164
Prathamesh Thakare <prathamthackeray18@gmail.com>
2026-03-11 01:22:53 +0530

    update log message

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-}
-);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server started on port 3000");
+});
